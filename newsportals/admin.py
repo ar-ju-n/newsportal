d1b78fca5fd6921a5ba5a19668b7d1ff305a2af9
ar-ju-n <att560112@gmail.com>
2024-05-08 14:27:26 +0545

    herageda

--- newsportals/admin.py
+++ newsportals/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import News, Category, Tag
 
-# Register your models here.
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title_en', 'title_np', 'slug', 'publish_date', 'category', 'status')
+    list_filter = ('category', 'tags', 'status')
+    search_fields = ('title_en', 'title_np', 'description_en', 'description_np')
+    prepopulated_fields = {'slug': ('title_en',)}
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name_en', 'name_np', 'slug', 'status')
+    list_filter = ('status',)
+    search_fields = ('name_en', 'name_np')
+    prepopulated_fields = {'slug': ('name_en',)}
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name_en', 'name_np', 'slug')
+    search_fields = ('name_en', 'name_np')
+    prepopulated_fields = {'slug': ('name_en',)}
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
