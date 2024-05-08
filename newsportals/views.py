a8605c02ff69868c59deee56818b935263617c1d
ar-ju-n <att560112@gmail.com>
2024-05-08 12:36:03 +0545

    v.0001

--- newsportals/views.py
+++ newsportals/views.py
@@ -1,20 +1,19 @@
-from django.shortcuts import render
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import News, Category, Tag
 from .forms import NewsForm
 
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
 
 def news_list(request):
     news = News.objects.filter(status='active')
     context = {'news': news}
-    return render(request, 'news/news_list.html', context)
+    return render(request, 'news_list.html', context)
 
 def news_detail(request, slug):
     news = get_object_or_404(News, slug=slug)
     context = {'news': news}
-    return render(request, 'news/news_detail.html', context)
+    return render(request, 'news_detail.html', context)
 
 def news_create(request):
     if request.method == 'POST':
@@ -27,4 +26,18 @@ def news_create(request):
     else:
         form = NewsForm()
     context = {'form': form}
-    return render(request, 'news/news_form.html', context)
+    return render(request, 'news_form.html', context)
+
+def news_update(request, slug):
+    news = get_object_or_404(News, slug=slug)
+    if request.method == 'POST':
+        form = NewsForm(request.POST, request.FILES, instance=news)
+        if form.is_valid():
+            news = form.save(commit=False)
+            news.save()
+            form.save_m2m()  # Save many-to-many relationships
+            return redirect('news_detail', slug=news.slug)
+    else:
+        form = NewsForm(instance=news)
+    context = {'form': form}
+    return render(request, 'news_form.html', context)
